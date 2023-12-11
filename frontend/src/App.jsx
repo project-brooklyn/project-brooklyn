3dfbfe3ea0f42bf8ef04758d2f36cf3c5981f9c6
brian <brianrlam@gmail.com>
2023-12-11 11:43:28 -0500

    make basic react page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Project Broolyn</h1>
+      <div style={{width: 1200, height: 600, backgroundColor: 'gray'}}>
+        game
+      </div>
+    </>
+  )
+}
+
+export default App

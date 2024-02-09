5842e445eccae270c95eb16e691582f557be5892
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-02-09 18:21:38 -0500

    Update frontend/src/enemies/Guy.js
    Co-authored-by: Kwame Wright <rockokw@gmail.com>

--- frontend/src/enemies/Guy.js
+++ frontend/src/enemies/Guy.js
@@ -2,9 +2,7 @@ import Enemy from "./Enemy";
 
 class Guy extends Enemy {
     constructor () {
-        super();
-        this.scale = 0.25;
-        this.modelFileLocation = './guy.glb';
+        super('./guy.glb', 0.25);
     }
 }
 

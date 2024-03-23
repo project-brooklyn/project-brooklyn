69707bc9217656923b84b3678a609bbf9a1cca57
Kwame Wright <rockokw@gmail.com>
2024-03-23 16:23:35 -0700

    Initial HpView component and demo with castle

---
+++ frontend/src/components/BaseView.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { useLoader } from '@react-three/fiber'
+import HpView from './HpView.jsx'
+
+export default function BaseView({base, hp})
+{
+    const model = useLoader(GLTFLoader, '/castle_3d_model.glb');
+    if (!model) {
+        return <>
+        </>
+    }
+
+    const [currentHp, setCurrentHp] = useState(hp)
+    useEffect(() => {
+        setCurrentHp(hp)
+    }, [hp])
+
+    // TODO: Use render util to convert coordinates, i.e. convertToRenderCoordinates().
+    model.scene.position.set(base.x, base.z * 0.1, base.y)
+
+    model.scene.rotation.set(0, base.rotation, 0);
+    model.scene.scale.set(base.scale, base.scale, base.scale)
+
+    const object = model.scene
+    const key = `${base.x},${base.y},${base.z}`
+
+    return <>
+        <HpView hp={currentHp} />
+        <primitive object={object} key={key} />
+    </>
+}
--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,5 +1,8 @@
-import { OrbitControls, PerspectiveCamera, Text } from '@react-three/drei'
+import { useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera, Stats, Text } from '@react-three/drei'
 
+import BaseView from './BaseView.jsx'
 import MapView from './MapView.jsx'
 import DemoMap from '../map/DemoMap.js'
 import Castle from '../entities/Castle.js';
@@ -7,18 +10,29 @@ import Portal from '../entities/Portal.js';
 import Guy from '../entities/Guy.js';
 import { randomInt } from '../utils/math_utils.js';
 
+const castle = new Castle(0,0,1);
+
 export default function Display()
 {
     const gameMap = new DemoMap();
 
-    const castle = new Castle(0,1,0);
     const portal = new Portal(1,1,1);
     const guys = [];
     for (let i = 0; i < 10; i++) {
         guys.push(new Guy(randomInt(2,9), 1.25, randomInt(2,9)));
     }
 
+    const [baseHp, setBaseHp] = useState(castle.hp)
+    useFrame(() =>
+    {
+        // For demonstration purposes.
+        castle.hp -= 1
+        if (castle.hp < 0) castle.hp = castle.maxHp
+        setBaseHp(castle.hp / castle.maxHp)
+    })
+
     return <>
+        <Stats />
         <PerspectiveCamera makeDefault fov={50} position={ [15, 10, 15] } />
         <OrbitControls />
 
@@ -27,9 +41,9 @@ export default function Display()
         </Text>
         <ambientLight intensity={ 2 } />
 
-        {castle.fiberComponent()}
-        {portal.fiberComponent()}
-        {guys.map(guy => guy.fiberComponent())}
+        <BaseView base={castle} hp={baseHp}/>
+        {/* {portal.fiberComponent()} */}
+        {/* {guys.map(guy => guy.fiberComponent())} */}
 
         <MapView gameMap={gameMap}/>
     </>

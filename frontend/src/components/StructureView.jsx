f52a4e4f95d15eba714ea6fc101620657494fd4d
brian <brianrlam@gmail.com>
2024-04-27 20:40:32 -0400

    center castle and fix names

--- frontend/src/components/StructureView.jsx
+++ frontend/src/components/StructureView.jsx
@@ -1,6 +1,6 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { convertToRenderCoordinates, modelFiles } from "../utils/render_utils";
+import { centerObjModel, convertToRenderCoordinates, modelFiles } from "../utils/render_utils";
 import { useLoader } from "@react-three/fiber";
 import HpView from "./HpView";
 
@@ -12,14 +12,16 @@ const renderStructure = (structure) => {
     const modelFile = modelFiles[name];
     const isObjModel = modelFile.slice(-3) === 'obj';
     
-    const modelLoader = useLoader(!isObj ? GLTFLoader : OBJLoader, modelFile)
+    const modelLoader = useLoader(!isObjModel ? GLTFLoader : OBJLoader, modelFile)
+    if (isObjModel) centerObjModel(modelLoader);
     const key = name + coordinates.x + coordinates.y + coordinates.z;
+    
     return (
         <group position={[coordinates.x, coordinates.y, coordinates.z]} >
             {hp && hp !== Infinity && <HpView hpFraction={hp / maxHp} position={[0, 0.5, 0]} width={1.0} />}
             <primitive
                 key={key}
-                object={isObj ? gltf : gltf.scene.clone(true)}
+                object={isObjModel ? modelLoader : modelLoader.scene.clone(true)}
                 scale={scale}
             />
         </group>

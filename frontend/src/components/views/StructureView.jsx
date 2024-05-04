daf4df392aad48538fbfca72bf9e2700ba624e80
Brian <brianrlam@gmail.com>
2024-05-04 11:29:05 -0400

    resolve merge issue with obj branch

--- frontend/src/components/views/StructureView.jsx
+++ frontend/src/components/views/StructureView.jsx
@@ -1,5 +1,6 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { convertToRenderCoordinates, modelFiles } from "../../utils/render_utils";
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { centerObjModel, convertToRenderCoordinates, modelFiles } from "../../utils/render_utils";
 import { useLoader } from "@react-three/fiber";
 import HpView from "./HpView";
 
@@ -8,21 +9,23 @@ const renderStructure = (structure) => {
     if (!hp) return null;
     const coordinates = convertToRenderCoordinates(structure, offset);
 
-    const gltf = useLoader(GLTFLoader, modelFiles[name]);gltf.scene.traverse((child) => {
-        if (child.isMesh) {
-            child.material = child.material.clone();
-            child.material.transparent = false;
-        }
-    });
-
+    const modelFile = modelFiles[name];
+    const isObjModel = modelFile.slice(-3) === 'obj';
+    
+    const modelLoader = useLoader(!isObjModel ? GLTFLoader : OBJLoader, modelFile)
+    if (isObjModel) centerObjModel(modelLoader);
     const key = name + coordinates.x + coordinates.y + coordinates.z;
-    return (<group position={[coordinates.x, coordinates.y, coordinates.z]} key={key}>
-        {hp && hp !== Infinity && <HpView hpFraction={hp / maxHp} position={[0, 0.5, 0]} width={1.0} />}
-        <primitive
-            object={gltf.scene.clone(true)}
-            scale={scale}
-        />
-    </group>);
+    
+    return (
+        <group position={[coordinates.x, coordinates.y, coordinates.z]} >
+            {hp && hp !== Infinity && <HpView hpFraction={hp / maxHp} position={[0, 0.5, 0]} width={1.0} />}
+            <primitive
+                key={key}
+                object={isObjModel ? modelLoader : modelLoader.scene.clone(true)}
+                scale={scale}
+            />
+        </group>
+    );
 };
 
 export default function StructureView({ structures }) {

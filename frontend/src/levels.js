0617c60ab375ed4cfe22f0efd3379e977ecae2e1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-05-04 11:12:58 -0400

    Implement Build Menu (#24)
    * fix key error
    
    * add buy menu
    
    * add build command, needs ton of cleanup
    
    * refactor game.towers
    
    * add gold reward, finish builTower except ghost icon
    
    * finish ghost buy view
    
    * add rectangular test, fix check for existing tower

--- frontend/src/levels.js
+++ frontend/src/levels.js
@@ -5,30 +5,36 @@ export const levels = [
         enemy: Guy,
         count: 0,
         delay: 1000,
+        gold: 100,
     },
     {
         enemy: Guy,
         count: 10,
         delay: 1000,
+        gold: 100,
     },
     {
         enemy: Guy,
         count: 20,
         delay: 1000,
+        gold: 100,
     },
     {
         enemy: Guy,
         count: 30,
         delay: 1000,
+        gold: 100,
     },
     {
         enemy: Guy,
         count: 40,
         delay: 1000,
+        gold: 100,
     },
     {
         enemy: Guy,
         count: 50,
         delay: 1000,
+        gold: 100,
     },
 ];

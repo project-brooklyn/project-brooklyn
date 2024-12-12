d56351c8efb996f7291ce1862145e35f5a1fc174
Brian <brianrlam@gmail.com>
2024-12-12 18:45:50 -0500

    add basic level info

--- frontend/src/entities/enemies/Guy.js
+++ frontend/src/entities/enemies/Guy.js
@@ -2,10 +2,11 @@ import Enemy from "./Enemy";
 
 export default class Guy extends Enemy {
     static SPEED = 2;
+    static MAX_HP = 100;
     constructor (x, y, z, scale) {
         super(x, y, z, scale ?? 0.01);
         this.name = "guy";
-        this.maxHp = 100;
+        this.maxHp = Guy.MAX_HP;
         this.hp = this.maxHp;
     }
 }

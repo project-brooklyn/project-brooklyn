cd9c766fc32a578e7511aa4c315d37059755c32d
Brian Lam <brianrlam@gmail.com>
2025-01-06 18:44:04 -0500

    conver tower buffs from array to object

--- frontend/src/components/ui/SelectedTowerInfo.jsx
+++ frontend/src/components/ui/SelectedTowerInfo.jsx
@@ -29,7 +29,7 @@ export const SelectedTowerInfo = ({selectedTower, purchasingItem, setSelectedTow
         <ul>
             <li>Type: {name}</li>
             <li>Price: {price}</li>
-            <li>Damage: {buffs.includes(BUFFED) ? damage * 2 : damage}</li>
+            <li>Damage: {buffs[BUFFED] ? damage * 2 : damage}</li>
             <li>Cooldown: {cooldown}</li>
             {minRange 
                 ? <li>Range: {minRange} - {maxRange}</li>

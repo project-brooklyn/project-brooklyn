23666ad7687b1601397e53fdd573a935f1959d99
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2025-02-13 09:58:30 -0500

    Update frontend/src/entities/projectiles/Dollar.js
    Co-authored-by: Kwame Wright <rockokw@gmail.com>

--- frontend/src/entities/projectiles/Dollar.js
+++ frontend/src/entities/projectiles/Dollar.js
@@ -4,7 +4,7 @@ import Projectile from "./Projectile";
 export default class Dollar extends Projectile {
     constructor(x, y, z, path) {
         const quaternion = new THREE.Quaternion();
-        super(x, y, z,  quaternion, path);
+        super(x, y, z, quaternion, path);
         this.name = "dollar";
     }
 }

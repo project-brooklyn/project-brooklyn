4aaae12ef8e51babbe6a5da816448a42f6bcd4b8
Kwame Wright <rockokw@gmail.com>
2025-03-19 17:18:06 -0700

    Fix status icon flickering

--- frontend/src/components/textures.js
+++ frontend/src/components/textures.js
@@ -7,6 +7,11 @@ const textures = {
     dirt: textureLoader.load('/textures/dirt.png'),
     stone: textureLoader.load('/textures/stone.png'),
     bedrock: textureLoader.load('/textures/bedrock.png'),
+    // TODO: Convert to lowercase
+    BUFFED: textureLoader.load('/icons/buff.png'),
+    BURNED: textureLoader.load('/icons/fire.png'),
+    FROZEN: textureLoader.load('/icons/ice.png'),
+    SLOWED: textureLoader.load('/icons/snail.png'),
 }
 
 export default textures;

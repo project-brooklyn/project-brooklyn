ee97f1bf2df8c87c43f5af7b4e22a95fb1d56969
Kwame Wright <rockokw@gmail.com>
2025-03-24 18:34:48 -0400

    Cleanup statuses and buffs (#128)
    Follow-up to #124

--- frontend/src/entities/towers/BuffTower.js
+++ frontend/src/entities/towers/BuffTower.js
@@ -1,13 +1,13 @@
 import { getAdjacentTilePath } from "../../utils/game_utils";
 import Tower from "./Tower";
 
-export const BUFFED = "BUFFED";
+export const BUFFED = "buffed";
 const SCALE = 0.02;
 
 export default class BuffTower extends Tower {
     static price = 100;
-    
-    constructor (x, y, z, status) {
+
+    constructor(x, y, z, status) {
         super(x, y, z, status, SCALE);
         this.name = 'buffTower';
         this.price = BuffTower.price;

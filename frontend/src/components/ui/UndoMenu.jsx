a34781c72022fa468281692d59930ddaa6973472
Kwame Wright <rockokw@gmail.com>
2025-04-16 12:54:43 -0700

    Revert UndoMenu change

--- frontend/src/components/ui/UndoMenu.jsx
+++ frontend/src/components/ui/UndoMenu.jsx
@@ -1,11 +1,11 @@
 export const UndoMenu = ({ game }) => {
     const { undoManager } = game;
     return <>
-        <div>
-            <div>
+        <div className="d-flex">
+            <div className="d-flex w-50 justify-content-center">
                 {undoManager.hasUndos() && <button onClick={undoManager.undo}>Undo</button>}
             </div>
-            <div>
+            <div className="d-flex w-50 justify-content-center">
                 {undoManager.hasRedos() && <button onClick={undoManager.redo}>Redo</button>}
             </div>
         </div>

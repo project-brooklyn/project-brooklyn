3039c3aa39cec21e6961256f03dc0b62bb63ecae
Kwame Wright <rockokw@gmail.com>
2025-05-14 19:21:23 -0400

    HUD for level, gold, phase (#154)
    ![image](https://github.com/user-attachments/assets/c9186dd6-9a47-4234-af40-a509a54ce6b2)
    
    Part of #142

--- frontend/src/components/ui/HtmlUI.jsx
+++ frontend/src/components/ui/HtmlUI.jsx
@@ -1,40 +1,24 @@
-import { useState } from "react";
 import { BUILD, DEFEND } from "../../Game";
 import { LevelInfo } from "./LevelInfo";
 import { ItemInfo } from "./ItemInfo";
 import { Box, Stack } from "@mui/material";
 
 export const HtmlUI = ({ game, selectedTower }) => {
-    const [level, setLevel] = useState(game.level);
-    const [gold, setGold] = useState(game.gold);
-    const [phase, setPhase] = useState(game.phase);
-
-    setInterval(() => {
-        setLevel(game.level);
-        setGold(game.gold);
-        setPhase(game.phase);
-    }, 100); // substitute for useFrame, which can't be used outside Canvas
-
     return <Box sx={{ width: "25vw", p: 2 }} >
         <Stack spacing={2}>
-            <Stack direction="row" sx={{ justifyContent: "space-between" }}>
-                <span>{`Level: ${level}`}</span>
-                <span>{`Gold: ${gold}`}</span>
-                <span>{`Phase: ${phase.toUpperCase()}`}</span>
-            </Stack>
-            {
-                game.enableTowerLimits && <Stack>
+            {game.enableTowerLimits &&
+                <Stack>
                     <span>{`Towers: ${game.getTowerCount()}`}</span>
                     <span>{`Tower Limit: ${game.towerLimit}`}</span>
                 </Stack>
             }
 
-            {phase === BUILD &&
+            {game.phase === BUILD &&
                 <Stack>
                     <ItemInfo item={selectedTower} />
                 </Stack>
             }
-            {phase === DEFEND &&
+            {game.phase === DEFEND &&
                 <Stack>
                     <LevelInfo game={game} />
                     <ItemInfo item={selectedTower} />

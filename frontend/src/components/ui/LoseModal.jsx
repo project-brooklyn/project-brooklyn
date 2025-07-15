e6bd9e0cbdbd0d3afedee8646971d2304d8129e6
Kwame Wright <rockokw@gmail.com>
2025-07-15 14:06:50 -0400

    Migrate modals to MUI (#164)
    Part of #142
    
    <img width="647" height="235" alt="image"
    src="https://github.com/user-attachments/assets/414af689-4187-4e1b-87ed-e4b959283ffa"
    />
    
    <img width="637" height="899" alt="image"
    src="https://github.com/user-attachments/assets/c94dd8fd-974a-464f-b30a-760ce32237b5"
    />
    
    <img width="628" height="269" alt="image"
    src="https://github.com/user-attachments/assets/0ec50181-1364-4f44-bbec-6a670751c832"
    />
    
    <img width="630" height="271" alt="image"
    src="https://github.com/user-attachments/assets/a45a42a3-906b-4d2b-bc5f-8d3e57f4cb1e"
    />
    
    <img width="623" height="235" alt="image"
    src="https://github.com/user-attachments/assets/77a18452-e024-46e1-9dce-240d1d7152ea"
    />

--- frontend/src/components/ui/LoseModal.jsx
+++ frontend/src/components/ui/LoseModal.jsx
@@ -1,34 +1,37 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import { useGameContext } from '../game/GameContext';
 
-function LoseModal({ show, onHide }) {
+function LoseModal({ show, hideModal }) {
     const game = useGameContext();
     return (
-        <Modal
-            show={show}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+        <Dialog
+            open={show}
+            maxWidth="sm"
+            fullWidth
         >
-            <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Game Over
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p align="center">
+            <DialogTitle>
+                Game Over
+            </DialogTitle>
+
+            <DialogContent>
+                <DialogContentText sx={{ textAlign: 'center' }}>
                     Your castle has fallen <br />
                     :( <br />
                     <br />
                     You made it to level {game.level - 1},
                     please try again.
-                </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+                </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button variant="contained" onClick={hideModal}>Close</Button>
+            </DialogActions>
+        </Dialog>
     );
 }
 
